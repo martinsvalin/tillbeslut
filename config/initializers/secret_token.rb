e247bf299c4a903c6f1317ac5d03c856bd97b067
Martin Svalin <martin@lite.nu>
2012-05-29 12:35:44 +0200

    Initial import.
    Generated with rails new votering -m http://railswizard.org/0815a16678d33e213369.rb -J -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Votering::Application.config.secret_token = '444d608cbf14eb97e41dfe2276d94c6eb877f8e1b39e3706e2d29d19b06e25841c69831e7a78f28849af245d5929eb14f2ef806114fbc7de1ad3ef640e0855a6'
