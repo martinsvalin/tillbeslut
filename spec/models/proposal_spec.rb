1e098b1fd3c9b1582a631f912a1c046d22e05be4
Martin Svalin <martin@lite.nu>
2012-07-14 23:20:54 +0200

    Validations on Proposal

--- spec/models/proposal_spec.rb
+++ spec/models/proposal_spec.rb
@@ -6,8 +6,24 @@ require_relative '../../app/models/proposal'
 describe Proposal do
   it "requires a header" do
     proposal = Proposal.new header: nil
-    proposal.should_not be_valid
+    proposal.valid?
+    proposal.errors[:header].should_not be_blank
   end
-  it "requires a number"
-  it "needs a unique number"
+
+  it "requires a number" do
+    proposal = Proposal.new number: nil
+    proposal.valid?
+    proposal.errors[:number].should_not be_blank
+  end
+
+  context "with another numbered proposal" do
+    before { Proposal.create header: 'Yep', number: '1.1' }
+    after { Proposal.delete_all }
+
+    it "needs a unique number" do
+      proposal = Proposal.new number: '1.1'
+      proposal.valid?
+      proposal.errors[:number].should_not be_blank
+    end
+  end
 end
