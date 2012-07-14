8cfb30d471e00e4c2b023f05c0129b11e628d97a
Martin Svalin <martin@lite.nu>
2012-07-14 21:48:54 +0200

    Restructuring spec to avoid duplication

--- spec/requests/proposals_spec.rb
+++ spec/requests/proposals_spec.rb
@@ -1,20 +1,23 @@
 require 'spec_helper'
 
 describe "proposals index page" do
-  it "can be reached at /proposals" do
-    visit '/proposals'
-    page.should have_content('Motioner')
+  context "at /motioner" do
+    it "can be reached" do
+      visit '/motioner'
+      page.should have_content('Motioner')
+    end
   end
 
-  it "can also be reached at /motioner" do
-    visit '/motioner'
-    page.should have_content('Motioner')
-  end
-
-  context "with no proposals" do
+  context "at /proposals" do
     before { visit '/proposals' }
-    it "tells us there are no proposals" do
-      page.should have_content('Inga motioner')
+    it "can be reached" do
+      page.should have_content('Motioner')
+    end
+
+    context "with no proposals" do
+      it "tells us there are no proposals" do
+        page.should have_content('Inga motioner')
+      end
     end
   end
 end
