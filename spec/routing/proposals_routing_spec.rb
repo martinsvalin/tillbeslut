be6fe42486bde6867bc0d5077a0a87a2e3a71f0a
Martin Svalin <martin@lite.nu>
2012-07-14 20:26:07 +0200

    Minor change for consistency

--- spec/routing/proposals_routing_spec.rb
+++ spec/routing/proposals_routing_spec.rb
@@ -4,7 +4,7 @@ describe "routes for proposals" do
   it "routes /proposals to proposals index" do
     get('/proposals').should route_to "proposals#index"
   end
-  it "routes /motioner to proposals#index" do
+  it "routes /motioner to proposals index" do
     get('/motioner').should route_to "proposals#index"
   end
 end
