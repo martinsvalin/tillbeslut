b5eaba52f24f7e03a9b752e65086f6dcf63ac952
Martin Svalin <martin@lite.nu>
2012-07-14 23:51:35 +0200

    Set @proposals in index action

--- spec/controllers/proposals_controller_spec.rb
+++ spec/controllers/proposals_controller_spec.rb
@@ -3,4 +3,19 @@ require_relative '../../app/controllers/application_controller'
 require_relative '../../app/controllers/proposals_controller'
 
 describe ProposalsController do
+  let(:controller) { ProposalsController.new }
+  before(:all) { Proposal = Class.new unless defined? Proposal }
+
+  describe 'index' do
+    it "asks for all the proposals" do
+      Proposal.should_receive(:all)
+      controller.index
+    end
+
+    it "sets @proposals instance variable" do
+      Proposal.stub(all: [1,2])
+      controller.index
+      controller.instance_variable_get(:@proposals).should eq [1,2]
+    end
+  end
 end
