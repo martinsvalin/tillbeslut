0de0914532d7860a33162fb6d07b96d25e913f6f
Martin Svalin <martin@lite.nu>
2012-07-15 00:14:25 +0200

    Show proposals on page
    Factories need to setup proposals before we visit
    the page. Restructured spec, some duplication
    necessary.

--- spec/requests/proposals_spec.rb
+++ spec/requests/proposals_spec.rb
@@ -1,35 +1,33 @@
 require 'spec_helper'
 
 describe "proposals index page" do
-  context "at /motioner" do
-    it "can be reached" do
-      visit '/motioner'
-      page.should have_content('Motioner')
+  it "can be reached" do
+    visit '/motioner'
+    page.should have_content('Motioner')
+  end
+
+  it "can be reached" do
+    visit '/proposals'
+    page.should have_content('Motioner')
+  end
+
+  context "with no proposals" do
+    it "tells us there are no proposals" do
+      visit '/proposals'
+      page.should have_content('Inga motioner')
     end
   end
 
-  context "at /proposals" do
-    before { visit '/proposals' }
-    it "can be reached" do
-      page.should have_content('Motioner')
+  context "with a couple of proposals" do
+    before do
+      FactoryGirl.create(:proposal, number: '1.1')
+      FactoryGirl.create(:proposal, number: '1.2')
+      visit '/proposals'
     end
 
-    context "with no proposals" do
-      it "tells us there are no proposals" do
-        page.should have_content('Inga motioner')
-      end
-    end
-
-    context "with a couple of proposals" do
-      before do
-        FactoryGirl.create(:proposal, number: '1.1')
-        FactoryGirl.create(:proposal, number: '1.2')
-      end
-
-      it "shows a numbered list of proposals" do
-        page.should have_content('Motion 1.1')
-        page.should have_content('Motion 1.2')
-      end
+    it "shows a numbered list of proposals" do
+      page.should have_content('Motion 1.1')
+      page.should have_content('Motion 1.2')
     end
   end
 end
