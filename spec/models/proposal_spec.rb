30099599b24319576ea6f1ab86648c361cf70dde
Martin Svalin <martin@lite.nu>
2012-08-01 19:09:19 +0200

    Dry specs

--- spec/models/proposal_spec.rb
+++ spec/models/proposal_spec.rb
@@ -1,46 +1,43 @@
 require 'spec_helper'
 
 describe Proposal do
+  let(:proposal) { Proposal.new }
+  let(:author) { Author.new }
+  let(:decision_point) { DecisionPoint.new }
+
   it "requires a header" do
-    proposal = Proposal.new header: nil
-    proposal.valid?
-    proposal.errors[:header].should_not be_blank
+    proposal.should_not be_valid
+    proposal.errors.added?(:header, :blank).should be_present
   end
 
   it "requires a number" do
-    proposal = Proposal.new number: nil
-    proposal.valid?
-    proposal.errors[:number].should_not be_blank
+    proposal.should_not be_valid
+    proposal.errors.added?(:number, :blank).should be_present
   end
 
   context "with another proposal in the database" do
     let(:existing_number) { '1.1' }
     before { FactoryGirl.create :proposal, number: existing_number }
-    after { Proposal.delete_all }
 
     it "needs a unique number" do
       proposal = Proposal.new number: existing_number
-      proposal.valid?
-      proposal.errors[:number].should_not be_blank
+      proposal.should_not be_valid
+      proposal.errors.added?(:number, :taken).should be_present
     end
   end
 
   it "has a list of authors" do
-    p = Proposal.new
-    author = Author.new
-    p.authors << author
-    p.authors.should eq [author]
+    proposal.authors << author
+    proposal.authors.should eq [author]
   end
 
   it "must have a list of decision points" do
-    p = FactoryGirl.build :proposal
-    p.decision_points = []
-    p.should_not be_valid
-    p.errors.added?(:decision_points, :blank).should be_present
+    proposal.decision_points = []
+    proposal.should_not be_valid
+    proposal.errors.added?(:decision_points, :blank).should be_present
 
-    decision_point = DecisionPoint.new
-    p.decision_points << decision_point
-    p.decision_points.should eq [decision_point]
-    p.should be_valid
+    proposal.decision_points << decision_point
+    proposal.valid?
+    proposal.errors[:decision_points].should be_blank
   end
 end
