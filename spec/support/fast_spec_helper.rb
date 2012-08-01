d51a70ced03552ac83d2b3c8b2c3aee1ce0c3626
Martin Svalin <martin@lite.nu>
2012-08-02 00:15:01 +0200

    Remove unused fast_spec_helper

--- spec/support/fast_spec_helper.rb
+++
@@ -1,7 +0,0 @@
-module FastSpecHelper
-  def require_active_record
-    require 'active_record'
-    config = YAML.load(File.open(File.expand_path(File.dirname(__FILE__)) << '/../../config/database.yml'))
-    ActiveRecord::Base.establish_connection(config['test'])
-  end
-end
