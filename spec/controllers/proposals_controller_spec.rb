1148c93e55bd433bed0d49b7d5c7b43a1e7fcf3a
Martin Svalin <martin@lite.nu>
2012-09-12 10:13:51 +0200

    Make xhr request in proposals controller spec

--- spec/controllers/proposals_controller_spec.rb
+++ spec/controllers/proposals_controller_spec.rb
@@ -2,15 +2,17 @@ require 'spec_helper'
 
 describe ProposalsController do
   describe 'index' do
-    it "asks for all the proposals" do
-      Proposal.should_receive(:all)
-      get :index
-    end
+    context "when request is xhr" do
+      it "asks for all the proposals" do
+        Proposal.should_receive(:all)
+        xhr :get, :index
+      end
 
-    it "sets @proposals instance variable" do
-      Proposal.stub(all: [1,2])
-      get :index
-      assigns[:proposals].should eq [1,2]
+      it "sets @proposals instance variable" do
+        Proposal.stub(all: [1,2])
+        xhr :get, :index
+        assigns[:proposals].should eq [1,2]
+      end
     end
   end
 end
